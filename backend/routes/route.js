ad6b858b5570c8add391e79ab955c63034bbc797
Deepanshu Bhatt <115899138+xAn-x@users.noreply.github.com>
2023-09-30 22:19:07 +0530

    Update route.js

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { userSignUp, userLogIn } from "../controller/user-controller.js";
-import {getProducts,getProductById} from "../Controller/product-controller.js";
+import {getProducts,getProductById} from "../controller/product-controller.js";
 
 const router = express.Router();
 
